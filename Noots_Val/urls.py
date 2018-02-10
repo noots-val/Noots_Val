2ac3a79b7bce4ac5cbcead62cdc4ba675d463015
noots-val <noots.val@gmail.com>
2018-02-11 01:20:52 +0900

    robots.txtの設定

--- Noots_Val/urls.py
+++ Noots_Val/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^official_site/', include('official_site.urls')),
-    url(r'^robots.txt/', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    url(r'^robots.txt/', TemplateView.as_view(template_name='common/robots.txt', content_type='text/plain')),
 ]
