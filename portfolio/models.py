5ab8187e37e425d31f4a0117366fd83a93466919
noots-val <noots.val@gmail.com>
2018-02-20 22:58:59 +0900

    コモディティテーブルを作成

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Commodity(models.Model):
+    # id = AutoField(primary_key=True)  # 自動定義
+    commodity_title = models.CharField(max_length=50)
+    image_url = models.URLField
+    category = models.CharField(max_length=20)
+    price = models.PositiveIntegerField
+    special_flg = models.BooleanField
+    favorite_flg = models.BooleanField
+    description = models.TextField
+    recommend1 = models.PositiveIntegerField(null=True)
+    recommend2 = models.PositiveIntegerField(null=True)
