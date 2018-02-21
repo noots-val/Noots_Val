f2e1132469065be3c214aa99e89eb4a76c58f052
noots-val <noots.val@gmail.com>
2018-02-21 23:13:37 +0900

    テーブル追加

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,11 +4,12 @@ from django.db import models
 class Commodity(models.Model):
     # id = AutoField(primary_key=True)  # 自動定義
     commodity_title = models.CharField(max_length=50)
-    image_url = models.URLField
+    image_url = models.URLField()
     category = models.CharField(max_length=20)
-    price = models.PositiveIntegerField
-    special_flg = models.BooleanField
-    favorite_flg = models.BooleanField
-    description = models.TextField
+    price = models.PositiveIntegerField()
+    special_flg = models.BooleanField()
+    favorite_flg = models.BooleanField()
+    description = models.TextField(max_length=1000)
+    stock = models.PositiveIntegerField()
     recommend1 = models.PositiveIntegerField(null=True)
     recommend2 = models.PositiveIntegerField(null=True)
