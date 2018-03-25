ad0fe3f301c4ab3e26d1d5a1127996eab4094059
noots-val <noots.val@gmail.com>
2018-03-25 10:54:07 +0900

    アカウント機能を拡張、マイページ実装（デザインはこれから）

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls import url
 app_name = 'accounts'
 urlpatterns = [
     url(r'^signup/$', views.SignUpView.as_view(), name='signup'),
+    url(r'^mypage/$', views.MypageView.as_view(), name='mypage'),
 ]
